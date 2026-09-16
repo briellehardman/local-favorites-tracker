7545b05a6cd254bc50a9d373d3ee7ea1c0803363
briellehardman <byhardman@gmail.com>
2026-09-16 11:35:53 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('app.js connected');
+let myFavorite = {
+    name: 'Zolis',
+    category: 'restaurant',
+    rating: 5,
+    notes: 'One of my favorite places',
+    dateAdded: 'today'
+};
+
+console.log(myFavorite.name);
+let displayText = myFavorite.name + ' - Rating: ' + myFavorite.rating + '/5';
+
+let today = new Date().toLocaleDateString();
+console.log(today);                     
+
+console.log(myFavorite);                 
+console.log(typeof myFavorite.name);     
+console.log(typeof myFavorite.rating);   
+
+let placeName = 'Zolis';
+let rating = 5;
+
+console.log(placeName + ' - ' + rating + '/5');
+console.log('⭐'.repeat(rating) + ' ' + placeName);
