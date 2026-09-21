f2a97c530be9fefa931ce82e8194ebf6d7d9fe68
briellehardman <byhardman@gmail.com>
2026-09-21 10:00:12 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ console.log('⭐'.repeat(rating) + ' ' + placeName);
 function greetFavorite(placeName, rating) {
     console.log(placeName + ' has ' + rating + ' stars!');
 }
-greetFavorite('Starbucks', 5);   // "Starbucks has 5 stars!"
+greetFavorite('Zolis', 5);   // "Zolis has 5 stars!"
 
 
 const nameInput = document.getElementById('name');
